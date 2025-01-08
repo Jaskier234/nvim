8e342b78957bdfa4b818394158faffa9898e5758
jaskier234 <jaskier234@gmail.com>
2025-01-08 17:27:24 +0100

    Formatting

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
             ensure_installed = { "c", "cpp", "bash", "git_config", "git_rebase", "gitcommit", "haskell", "lua", "make", "markdown", "ocaml", "python", "rust" },
             sync_install = false,
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
